88e18f9642f648caa45db875851faeb17fd7a8a7
houquanchang <745068179@qq.com>
2021-09-08 16:37:31 +0800

    第二次提交

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ app.get('/zhuye', (req, res) => {
 app.get('/zhuye2', (req, res) => {
     res.send({ name: 'laowang', age: 4 })
 })
+app.get('/zhuye3', (req, res) => {
+    res.send({ name: 'laowg', age: 88 })
+})
 app.listen(3000, err => {
     if (!err) console.log('服务器启动成功');
     else console.log(err);
